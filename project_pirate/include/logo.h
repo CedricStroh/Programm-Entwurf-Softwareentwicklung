bbfd385d024e06445d22d99ebf20eef9d90bbe23
Jannis-M <jannis.messer@gmail.com>
2025-03-14 09:56:16 +0100

    logo

---
+++ project_pirate/include/logo.h
@@ -0,0 +1,7 @@
+#ifndef LOGO_H
+#define LOGO_H
+
+// Function to display our group logo
+void print_complex_logo();
+
+#endif // LOGO_H
---
+++ project_pirate/src/logo.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "logo.h"
+
+// This function displays a complex ASCII art logo of a pirate ship (The best group logo of them all ARRRRRR!!)
+void print_complex_logo() {
+   printf("\n");
+   printf("         _~     \n");
+   printf("      _~ )_)_~  \n");
+   printf("      )_))_))_) \n");
+   printf("      _!__!__!_ \n");
+   printf("  \\ \\_________/ /  \n");
+   printf(" ~~~~~~~~~~~~~~~~~~ \n");
+   printf("\n");
+}
